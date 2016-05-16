5ed04ea72ca8fccc1aa2e4f2ac45e87292df82f5
Koh Okada <koh.okada@gmail.com>
2016-05-17 02:52:22 +0900

    fix buble_sort

--- bubble_sort.php
+++ bubble_sort.php
@@ -1,11 +1,11 @@
 <?
-		$arr = [3,5,1,2,7,0,8,14,3];
+		$arr = [19,3,5,1,2,7,0,8,14,3];
 		$sorted_arr = bubble_sort($arr);
 		var_dump($sorted_arr);
 
 		function bubble_sort($arr)
 		{
-			for ($i = 0; $i < count($arr); $i++) {
+			for ($i = 0; $i < count($arr) - 1; $i++) {
 				for ($j = 1; $j < count($arr); $j++ ) {
 					if ($arr[$j - 1] > $arr[$j]) {
 						$tmp = $arr[$j];
