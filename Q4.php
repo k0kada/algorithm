8d3c0aa416856520fa402823a7897397c6a8aa38
Koh Okada <koh.okada@gmail.com>
2016-05-21 00:21:43 +0900

    Q4別解

--- Q4.php
+++ Q4.php
@@ -2,6 +2,11 @@
   var_dump(cut(3, 20, 1));
   var_dump(cut(5, 100, 1));
 
+  var_dump(cut2(3, 20));
+  var_dump(cut2(5, 100));
+
+
+
   function cut($m, $n, $cut, $num = 0) {
     if ($cut >= $n) {
       return $num;
@@ -12,5 +17,21 @@
       $num++;
       return cut($m, $n, $cut + $m, $num);
     }
+  }
+
+  function cut2($m, $n) {
+    $num = 0;
+    $cut = 1;
+
+    while ($n > $cut) {
+      if ($cut < $m) {
+        $cut += $cut;
+        $num++;
+      } else {
+        $cut += $m;
+        $num++;
+      }
+    }
+    return $num;
   }
 ?>
